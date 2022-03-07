0be25b731b2f64a4ce4701262c2fe31a5876b9bf
Thomas Ngo <thomasngo818@gmail.com>
2022-03-07 21:32:06 +0000

    Add implementation of player class' attributes and methods

--- player.rb
+++ player.rb
@@ -1,7 +1,21 @@
 class Player 
+  attr_reader :name, :lives
+
   def initialize (name)
     @name = name
+    self.lives = 3
   end
+
+  def lose_life
+    # lives--
+    self.lives -= 1
+  end
+
+  private
+
+  attr_writer :lives
+
+
 end
 
 
