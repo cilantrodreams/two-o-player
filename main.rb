d25fea66b24a8d6e563a6fff478cc879fce59e84
Thomas Ngo <thomasngo818@gmail.com>
2022-03-07 23:14:22 +0000

    Add code to instantiate one game and run it

---
+++ main.rb
@@ -0,0 +1,16 @@
+require './player'
+require './problem'
+require './game'
+# player1 = Player.new ("Thomas")
+# player1.lose_life
+# player1.lose_life
+# player1.lose_life
+# puts player1.lives
+# puts player1.name
+
+# problem1 = Problem.new(6, 4)
+# puts problem1.read_problem
+# puts problem1.solution
+
+game = Game.new
+game.start_game
